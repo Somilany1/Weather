08e08a04fb405cf2ebebb16b7d945c4526f50f0a
somilany <bordyukovadascha@gmail.com>
2024-09-24 22:49:28 +0300

    Update query, add cities

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -14,9 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonShow: UIButton!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var tableView: UITableView!
-    var city = ["City", "City2", "City3"]
+    
+    var cities = [String: [String: Double]]()
+    var city:[String] = []
     
     override func viewDidLoad() {
+        setData()
         super.viewDidLoad()
         getWeatherButton.addTarget(self, action: #selector(didTapGetWeatherButton), for: .touchUpInside)
         tableView.isHidden = true
@@ -48,27 +51,51 @@ class ViewController: UIViewController {
         }
         }
     }
+    
+    func setData() {
+        cities["Minsk"] = ["latitude": 53.9, "longitude": 27.5667]
+        cities["Grodno"] = ["latitude": 53.6697, "longitude": 23.8298]
+        cities["Gomel"] = ["latitude": 52.4345, "longitude": 30.9754]
+        cities["Vitebsk"] = ["latitude": 55.1904, "longitude": 30.2049]
+        cities["Mogilev"] = ["latitude": 53.9, "longitude": 30.3456]
+        cities["Brest"] = ["latitude": 52.0976, "longitude": 23.7341]
         
+        city = Array(cities.keys)
+    }
         
     @objc func didTapGetWeatherButton() {
-            let urlString = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true"
-            let url = URL(string: urlString)!
-            let request = URLRequest(url:url)
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
-                    DispatchQueue.main.async {
-                        self.weatherLabel.text = "\(weather.currentWeather.temperature) °"
+            let selectedCity = cityDropDown.title(for: .normal)
+
+            if let city = selectedCity {
+                if let cityCoordinates = cities[city] {
+                    if let latitude = cityCoordinates["latitude"], let longitude = cityCoordinates["longitude"] {
+                        let query = "latitude=\(latitude)&longitude=\(longitude)"
+                        let urlString = "https://api.open-meteo.com/v1/forecast?\(query)&current_weather=true"
+
+                        let url = URL(string: urlString)!
+                        let request = URLRequest(url:url)
+                        
+                        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                                DispatchQueue.main.async {
+                                    self.weatherLabel.text = "\(weather.currentWeather.temperature) °"
+                                }
+                            } else {
+                                print("Fail!")
+                            }
+                        }
+                        task.resume()
+                    } else {
+                        print("Coordinates not found.")
                     }
-                } else {
-                    print("Fail!")
                 }
             }
-            task.resume()
         }
     }
     
     extension ViewController: UITableViewDelegate, UITableViewDataSource {
         func tableView (_ tableView: UITableView, numberOfRowsInSection:Int) -> Int {
+            cityDropDown.setTitle("\(city[0])", for: .normal)
             return city.count
         }
         func tableView (_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
