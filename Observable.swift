a65e5339eb131bc0242beefa770dd4b89eaf5ce6
somilany <bordyukovadascha@gmail.com>
2024-09-26 18:40:34 +0300

    Mutate Observable

--- Observable.swift
+++ Observable.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class Observable<T> {
+  typealias Observer = (T) -> Void
+  var observer: Observer?
+
+  func observe(_ observer: Observer?) {
+    self.observer = observer
+    observer?(value)
+  }
+
+  var value: T {
+    didSet {
+      observer?(value)
+    }
+  }
+
+  init(_ value: T) {
+    self.value = value
+  }
+}
